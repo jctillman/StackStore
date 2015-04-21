211d23e878e8262501f6d31b33cff1af7c65877d
ksliao <katrina.s.liao@gmail.com>
2015-04-21 15:51:47 -0400

    updated some states

---
+++ browser/js/categories/categories.js
@@ -0,0 +1,20 @@
+'use strict';
+app.config(function ($stateProvider) {
+
+    // Register our *about* state.
+    $stateProvider.state('categoriesHome', {
+        url: '/categoriesHome',
+        controller: 'CategoriesController',
+        templateUrl: 'js/categories/categories.html'
+    });
+
+});
+
+app.controller('CategoriesController', function ($scope) {
+
+    // Images of beautiful Fullstack people.
+    $scope.images = [
+     //images of products??
+    ];
+
+});
