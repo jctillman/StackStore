1bf86453fa32eff2510430c9f03903e0cd9f9aa2
Sonia Trehan <soniatrehan6@gmail.com>
2015-04-23 14:13:04 -0400

    Working on user states

--- browser/js/user/user.js
+++ browser/js/user/user.js
@@ -1,32 +1,19 @@
 app.config(function ($stateProvider) {
 
-    $stateProvider.state('user', {
-        url: '/:username',
-        template: '<img ng-repeat="item in stash" width="300" ng-src="{{ item }}" />',
-        controller: function ($scope, SecretStash) {
-            SecretStash.getStash().then(function (stash) {
-                $scope.stash = stash;
-            });
-        },
-        // The following data.authenticate is read by an event listener
-        // that controls access to this state. Refer to app.js.
-        data: {
-            authenticate: true
-        }
-    });
+    $stateProvider
+        .state('user', {
+            url: '/:username',
+            templateUrl: 'js/user/user.html',
+            controller: 'UserCtrl',
+            // The following data.authenticate is read by an event listener
+            // that controls access to this state. Refer to app.js.
+            data: {
+                authenticate: true
+            }
+        });
 
 });
 
-app.factory('SecretStash', function ($http) {
-
-    var getStash = function () {
-        return $http.get('/api/members/secret-stash').then(function (response) {
-            return response.data;
-        });
-    };
-
-    return {
-        getStash: getStash //inside server>routes>members>index.js
-    };
+app.controller('UserCtrl', function($scope){
 
 });
