a18eac97a1471ae46b48f8b8778763d0f6282bbe
ksliao <katrina.s.liao@gmail.com>
2015-04-24 16:35:35 -0400

    added product detail shell

--- browser/js/list/products/productDetail.js
+++ browser/js/list/products/productDetail.js
@@ -1,19 +1,50 @@
 app.config(function ($stateProvider) {
 
     // Register our *about* state.
-    $stateProvider.state('categoriesHome.productDetail', {
-        url: '/:productDetail',
+    $stateProvider.state('list.productDetail', {
+        url: '/:product',
         controller: 'ProductDetail',
-        templateUrl: 'js/categories/products/products.html'
+        templateUrl: 'js/list/products/products.html'
     });
 
 });
 
-app.controller('ProductDetail', function ($scope) {
+app.controller('ProductDetail', function ($scope, ProductPhoto, ProductInfo, $http, $stateParams) {
 
-    // Images of beautiful Fullstack people.
-    $scope.images = [
-     //images of products??
-    ];
+    $scope.product = $stateParams.product;
+
+    $scope.images = function(){
+        ProductPhoto.getPhotoUrl().then(function(photo){
+            $scope.photo = photo;
+        });
+    };
+    $scope.images();
+
+    $scope.description = function(){
+       ProductInfo.getProductInfo().then(function(info){
+        $scope.info = info;
+       });
+    };
 
 });
+
+app.factory('ProductPhoto', function($http){
+    return{
+        getPhotoUrl: function(){
+            return $http.get('/api/product'+ $stateParams.product).then(function(response){
+                return response.data;
+            });
+        }
+    };
+});
+
+app.factory('ProductInfo', function($http){
+     return{
+            getProductInfo: function(){
+                return $http.get('/api/product').then(function(response){
+                    return response.data;
+                });
+            }
+        };
+});
+
