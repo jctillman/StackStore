fe2e4c37e5010b59d02a13d684dd5a3c8e51d07b
ksliao <katrina.s.liao@gmail.com>
2015-04-26 15:18:07 -0400

    fixed merge conflicts for productDetails

--- browser/js/list/products/productDetail.js
+++ browser/js/list/products/productDetail.js
@@ -9,50 +9,6 @@ app.config(function ($stateProvider) {
 
 });
 
-<<<<<<< HEAD
-app.controller('ProductDetail', function ($scope, ProductPhoto, ProductInfo, $http, $stateParams) {
-
-    $scope.product = $stateParams.product;
-      
-
-});
-
-// app.factory('ProductPhoto', function($http){
-//     return{
-//         getPhotoUrl: function(){
-//             return $http.get('/api/product'+ $stateParams.product).then(function(response){
-//                 return response.data;
-//             });
-//         }
-//     };
-// });
-
-app.factory('ProductInfo', function($http){
-    return{
-            getProductInfo: function(){
-                return $http.get('/api/product/').then(function(response){
-                    return response.data;
-                });
-            },
-
-            product: {
-                title: "",
-                price: null,
-                categories: [],
-                photoUrls: [],
-                splashPhoto: null,
-                reviews: [],
-                promo: ''
-            },
-
-            category: {
-                type: '',
-                data: '',
-                order: null
-            }
-        };
-});
-=======
 app.controller('ProductDetail', function ($scope, getProduct, $http, $stateParams) {
      
      $scope.productFind = function(){
@@ -86,4 +42,3 @@ app.factory('getProduct', function($http, $stateParams){
         
     }
 });
->>>>>>> 53ad9ddcf3e4ef92618b0472b60a62812e2c422e
