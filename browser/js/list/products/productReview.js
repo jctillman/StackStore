bf1087d2af4fdebce450ff344e0d0f822646d70b
ksliao <katrina.s.liao@gmail.com>
2015-04-27 14:53:12 -0400

    completed user reviews. users can now add reviews

--- browser/js/list/products/productReview.js
+++ browser/js/list/products/productReview.js
@@ -9,25 +9,36 @@ app.config(function ($stateProvider) {
 
 });
 
-app.controller('ProductReview', function ($scope, ReviewFactory, $http, $stateParams) {
+app.controller('ProductReview', function ($scope, ReviewFactory, $http, $stateParams, Session) {
 
      $scope.productReview = function(){
         ReviewFactory.addReview().then(function(data){
             $scope.product = data;
+            $scope.review = data.review;
         });
      }
 
      $scope.productReview();
 
-    //  $scope.addReview = function(){
-    //     ReviewFactory.addProductReview().then(function(data){
-    //         $scope.review = data;
-    //     });
-    //  }
+     $scope.rating;
+     $scope.content;
 
-    // $scope.addReview();
 
-});
+     $scope.submitReview = function(product){
+        var review = {
+            user: Session.user._id,
+            product: $scope.product._id,
+            stars: $scope.rating,
+            content: $scope.content
+        }
+            ReviewFactory.createReview(review).then(function(data){
+                return(data);
+            })
+        
+        };
+     
+
+    });
 
 
 app.factory('ReviewFactory', function($http, $stateParams){
@@ -36,6 +47,11 @@ app.factory('ReviewFactory', function($http, $stateParams){
             return $http.put('/api/product/search/', + {title: $stateParams.product}).then(function(response){
                 return response.data;
             });
+        },
+        createReview: function(newReview){
+            return $http.post('/api/review/newReview', newReview).then(function(response){
+                return response.data;
+            });
         }
-    }
+    };
 });
