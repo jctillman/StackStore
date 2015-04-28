7042d5f2523e773a17e062b59901913836375565
James Tillman <james.tillman.7@gmail.com>
2015-04-28 14:27:16 -0400

    Misc

--- server/app/configure/authentication/index.js
+++ server/app/configure/authentication/index.js
@@ -55,6 +55,7 @@ module.exports = function (app) {
 
     // Simple /logout route.
     app.get('/logout', function (req, res) {
+        req.session.cart = null;
         req.logout();
         res.status(200).end();
     });
